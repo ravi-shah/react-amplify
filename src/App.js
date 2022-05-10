ddd11d1ec88173e38728a26613e11a8e6ba3fd0d
RAVI SHAH <ravishah@RAVIs-MacBook-Pro.local>
2022-05-10 12:01:26 -0400

    Added sample

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the Home Screen!
+          Welcome to the Dashboard!
         </p>
         
       </header>
