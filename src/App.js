99244f5f8f4856841b552837ab94fc0f24d54af3
RAVI SHAH <ravishah@RAVIs-MacBook-Pro.local>
2022-05-18 11:23:52 -0400

    Added environment variable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const BUILD_ENV = process.env.REACT_APP_API_KEY;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the Dashboard!
+          Welcome to the Dashboard! {BUILD_ENV}
         </p>
         
       </header>
